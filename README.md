f9ea4a139737afbfa6a64bedf1a55e6c33e1cb0e
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-25 15:24:29 -0500

    made leap and bounds it is now running on html and not jade

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Canvas Warriors
 
-## New DB Collections
+## Start the application
+
+Start the database:
+- mongod
+
+Start node:
+- npm start
+
+
+### New DB Collections
 In the console run these commands:
 - mongo
 - db.createCollection("collection-name")
--- app.js
+++ app.js
@@ -5,15 +5,20 @@ var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     db = require('./model/db'),
+    blob = require('./model/blobs'),
+    renderFile = require('ejs').renderFile,
     routes = require('./routes/index'),
-    users = require('./routes/users');
+    users = require('./routes/users'),
+    blobs = require('./routes/blobs');
 
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/blobs', blobs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
